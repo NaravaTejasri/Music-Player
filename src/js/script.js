1b92501cda615a747112343653cbd1d706ae706a
TejasriNarava <tejasrisony17@gmail.com>
2021-04-13 12:52:56 +0200

    Final commit

---
+++ src/js/script.js
@@ -0,0 +1,114 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+//song titles
+const songs = ['Kabhijo', 'Purelove', 'SoMuchToSay'];
+
+//Keep track of song
+
+let songIndex = 2;
+
+loadSong(songs[songIndex]);
+
+//update song 
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `music/songs/${song}.mp3`;
+    cover.src = `music/pictures/${song}.jpg`;
+}
+
+//Play song 
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+    audio.play();
+}
+//Pause song 
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+    audio.pause();
+}
+
+//Previous song
+function prevSong(){
+    songIndex--;
+    if(songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+//Next  song
+function nextSong(){
+    songIndex++;
+    if(songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+//update progress bar
+function updateProgress(e) {
+    //console.log(e.srcElement);
+    const {duration, currentTime} = e.srcElement;
+    //console.log(duration, currentTime);
+    const progressPercent = (currentTime / duration) * 100;
+    //console.log(progressPercent);
+    progress.style.width = `${progressPercent}%`;
+
+
+}
+
+// Set progress bar
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+  
+    audio.currentTime = (clickX / width) * duration;
+  }
+  
+
+
+//Event listeners
+
+playBtn.addEventListener('click', ()=> {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if (isPlaying) {
+        pauseSong();
+    }else {
+        playSong();
+    }
+});
+
+
+//Chnage songs
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+//time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+//Click on progress bar
+progressContainer.addEventListener('click',setProgress);
+
+//Song ends
+audio.addEventListener('ended', nextSong);
+
+
+
